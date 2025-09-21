2355fe007bdc5e7490680530233b2e145715fcef
Emafido Emmanuel <emmanuelemafido@gmail.com>
2025-09-21 14:23:19 +0100

    Update App.jsx

--- Bridge/src/App.jsx
+++ Bridge/src/App.jsx
@@ -1,16 +1,20 @@
+import {Routes,Route } from 'react-router-dom'
 import { useState } from 'react'
 import './App.css'
 import Homepage from './assets/components/Homepage'
 import Interface from './assets/components/Interface'
-import Settings from './assets/components/Settings'
-
 function App() {
 
   return (
-    <>
-    <Interface />
-     </>
-  )
+    <div>
+      
+      <Routes>
+        <Route path='/' element = {<Homepage />}/>
+        <Route path='/interface' element = {<Interface />}/>
+      </Routes>
+    
+    </div>
+  );
 }
 
 export default App
