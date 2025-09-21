0d4c0e0f59b98cf2bd7a463e11010f9aa99d6ad2
Emafido Emmanuel <emmanuelemafido@gmail.com>
2025-09-21 14:22:45 +0100

    Update Getstarted.jsx

--- Bridge/src/assets/components/Getstarted.jsx
+++ Bridge/src/assets/components/Getstarted.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Getstarted(){
   return(
     <>
@@ -5,7 +7,9 @@ export default function Getstarted(){
       <p className="text-center text-xl font-extrabold  sm:text-3xl lg:text-5xl">Ready to Experience Seamless Conversations?</p>
       <p className="text-center my-2 font-light lg:text-[1.3rem]">Join a growing community bridging voices and breaking barriers.</p>
       <div className="text-center">
+        <Link to="/Interface">
         <button className="border-[1px] p-2 rounded-[10px] shadow-2xl border-gray-400 mt-7 sm:px-4 lg:w-[200px] lg:h-[50px] cursor-pointer font-bold lg:text-[1.2rem] animate-bounce">Get Started</button>
+        </Link>
       </div>
     </div>
     </>
